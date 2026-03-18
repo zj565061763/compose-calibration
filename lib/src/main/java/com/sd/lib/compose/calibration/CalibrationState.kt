5870d0894549ee0d87c3ed262832e1336b9908a7
zjun <565061763@qq.com>
2026-03-18 12:11:57 +0800

    rememberCalibrationState

--- lib/src/main/java/com/sd/lib/compose/calibration/CalibrationState.kt
+++ lib/src/main/java/com/sd/lib/compose/calibration/CalibrationState.kt
@@ -1,33 +1,19 @@
 package com.sd.lib.compose.calibration
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 @Composable
 fun rememberCalibrationState(
   groups: List<CalibrationGroup>,
 ): CalibrationState {
-  return remember { CalibrationState() }.also { it.Init(groups) }
+  return remember(groups) { CalibrationState(groups = groups) }
 }
 
-class CalibrationState internal constructor() {
-  internal var stableGroups by mutableStateOf<List<CalibrationGroup>>(emptyList())
-    private set
-
-  @Composable
-  internal fun Init(groups: List<CalibrationGroup>) {
-    LaunchedEffect(groups) {
-      withContext(Dispatchers.Default) {
-        stableGroups = groups.map { it.toStableCalibrationGroup() }
-      }
-    }
-  }
+class CalibrationState internal constructor(
+  groups: List<CalibrationGroup>,
+) {
+  internal val stableGroups: List<CalibrationGroup> = groups.map { it.toStableCalibrationGroup() }
 
   /** 获取当前的标定组 (坐标为百分比) */
   fun getCurrentGroups(): List<CalibrationGroup> {
